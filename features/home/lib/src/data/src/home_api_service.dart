d3f6c700ae38d98931783ac57550a85c658c6410
AldiAlpaujan <aldi.m.alpaujan.2@gmail.com>
2025-05-31 16:48:40 +0700

    some update

--- features/home/lib/src/data/src/home_api_service.dart
+++ features/home/lib/src/data/src/home_api_service.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 import 'package:home/src/domain/models/response/product_res.dart';
 
@@ -7,8 +6,11 @@ part 'home_api_service.g.dart';
 
 @RestApi()
 abstract class HomeApiService {
-  @factoryMethod
-  factory HomeApiService(Dio dio) = _HomeApiService;
+  factory HomeApiService(
+    Dio dio, {
+    String? baseUrl,
+    ParseErrorLogger? errorLogger,
+  }) = _HomeApiService;
 
   @GET('/payment/package/categories')
   Future<HttpResponse<ProductRes>> getProducts();
